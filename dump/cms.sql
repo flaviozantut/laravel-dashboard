f2e36ae080543749adde3ac87006ee2b05edadd0
flaviozantut <webmaster@flaviozantut.com>
2012-09-09 22:26:03 -0300

    add dump database

---
+++ dump/cms.sql
@@ -0,0 +1,475 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost:3306
+-- Tempo de Geração: 09/09/2012 às 22h24min
+-- Versão do Servidor: 5.5.24
+-- Versão do PHP: 5.3.10-1ubuntu3.2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categorias`
+--
+
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cod` int(11) NOT NULL,
+  `nome` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentarios`
+--
+
+CREATE TABLE IF NOT EXISTS `comentarios` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `comentario` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentario_produto`
+--
+
+CREATE TABLE IF NOT EXISTS `comentario_produto` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `id_comentario` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `comentario_produto_id_comentario_unique` (`id_comentario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cores`
+--
+
+CREATE TABLE IF NOT EXISTS `cores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cod` int(11) DEFAULT NULL,
+  `nome` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `rgb` varchar(7) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estoques`
+--
+
+CREATE TABLE IF NOT EXISTS `estoques` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `id_tamanho` int(11) NOT NULL,
+  `id_cor` int(11) NOT NULL,
+  `qtd` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fotos`
+--
+
+CREATE TABLE IF NOT EXISTS `fotos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `alt` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `legenda` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `foto` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `permissions` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `groups_name_unique` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `laravel_migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `laravel_migrations` (
+  `bundle` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`bundle`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `laravel_migrations`
+--
+
+INSERT INTO `laravel_migrations` (`bundle`, `name`, `batch`) VALUES
+('application', '2012_08_04_042238_create_session_table', 1),
+('application', '2012_09_03_204350_pages', 1),
+('application', '2012_09_05_172452_produtos', 2),
+('application', '2012_09_06_125645_marcas', 2),
+('application', '2012_09_06_125719_categorizacao', 2),
+('application', '2012_09_06_125742_fotos', 2),
+('application', '2012_09_06_130324_comentarios', 2),
+('sentry', '2012_08_03_162320_install', 1),
+('sentry', '2012_08_15_001334_database_rules', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `marcas`
+--
+
+CREATE TABLE IF NOT EXISTS `marcas` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cod` int(11) NOT NULL,
+  `logo` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `nome` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `marcas`
+--
+
+INSERT INTO `marcas` (`id`, `cod`, `logo`, `nome`, `created_at`, `updated_at`) VALUES
+(1, 1, 'ewrwer.wer', 'sfgfdg', '2012-09-07 20:56:05', '2012-09-07 20:56:05'),
+(2, 123123, 'qwe', 'Marca 1', '2012-09-07 21:46:54', '2012-09-07 21:46:54'),
+(3, 1, 'a.jpg', 'teste', '2012-09-09 23:42:00', '2012-09-09 23:42:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `marca_produto`
+--
+
+CREATE TABLE IF NOT EXISTS `marca_produto` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `produto_id` int(11) NOT NULL,
+  `marca_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `produto_id` (`produto_id`,`marca_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=32 ;
+
+--
+-- Extraindo dados da tabela `marca_produto`
+--
+
+INSERT INTO `marca_produto` (`id`, `produto_id`, `marca_id`, `created_at`, `updated_at`) VALUES
+(15, 11, 2, '2012-09-08 21:58:05', '2012-09-08 21:58:05'),
+(25, 1, 2, '2012-09-09 19:53:02', '2012-09-09 19:53:02'),
+(26, 12, 2, '2012-09-09 19:53:17', '2012-09-09 19:53:17'),
+(31, 7, 3, '2012-09-10 00:47:57', '2012-09-10 00:47:57');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pages`
+--
+
+CREATE TABLE IF NOT EXISTS `pages` (
+  `slug` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `title` text COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  `others` text COLLATE utf8_unicode_ci,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  UNIQUE KEY `pages_slug_unique` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `pages`
+--
+
+INSERT INTO `pages` (`slug`, `title`, `text`, `others`, `created_at`, `updated_at`) VALUES
+('informacoes-de-entrega', 'asdsdsd - dsf', '<p>u</p>\r\n', NULL, '0000-00-00 00:00:00', '2012-09-07 20:49:53'),
+('sobre-a-emresa', 'as', '<p>as</p>', NULL, '0000-00-00 00:00:00', '2012-09-04 23:53:58');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cod` int(11) NOT NULL,
+  `sku` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `nome` text COLLATE utf8_unicode_ci NOT NULL,
+  `descricao` text COLLATE utf8_unicode_ci NOT NULL,
+  `especificacoes` text COLLATE utf8_unicode_ci NOT NULL,
+  `preco` decimal(5,2) NOT NULL,
+  `preco_promocional` decimal(5,2) DEFAULT NULL,
+  `slug` text COLLATE utf8_unicode_ci NOT NULL,
+  `peso` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `cod`, `sku`, `nome`, `descricao`, `especificacoes`, `preco`, `preco_promocional`, `slug`, `peso`, `created_at`, `updated_at`) VALUES
+(7, 1111, '999', 'teste', '<p>aaaa</p>', '<p>lalalaa</p>', 0.00, NULL, '', 0, '2012-09-08 21:49:48', '2012-09-10 01:16:32'),
+(12, 9991, '01', 'teste', '<p>teste</p>', '<p>teste</p>', 0.00, NULL, '', 0, '2012-09-08 22:07:24', '2012-09-09 19:53:17');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto_categorizacao`
+--
+
+CREATE TABLE IF NOT EXISTS `produto_categorizacao` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `id_subcategoria_categorias` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto_cores`
+--
+
+CREATE TABLE IF NOT EXISTS `produto_cores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `id_cor` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto_fotos`
+--
+
+CREATE TABLE IF NOT EXISTS `produto_fotos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) NOT NULL,
+  `id_foto` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `rules`
+--
+
+CREATE TABLE IF NOT EXISTS `rules` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `rule` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rules_rule_unique` (`rule`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `rules`
+--
+
+INSERT INTO `rules` (`id`, `rule`, `description`) VALUES
+(1, 'superuser', 'Access to Everything'),
+(2, 'is_admin', 'Administrative Privileges');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `last_activity` int(11) NOT NULL,
+  `data` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `subcategorias`
+--
+
+CREATE TABLE IF NOT EXISTS `subcategorias` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cod` int(11) NOT NULL,
+  `nome` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `subcategoria_categorias`
+--
+
+CREATE TABLE IF NOT EXISTS `subcategoria_categorias` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_categoria` int(11) NOT NULL,
+  `id_subcategoria` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tamanhos`
+--
+
+CREATE TABLE IF NOT EXISTS `tamanhos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cod` int(11) NOT NULL,
+  `nome` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_hash` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `temp_password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_me` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `activation_hash` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `ip_address` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `activated` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `permissions` text COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `password_reset_hash`, `temp_password`, `remember_me`, `activation_hash`, `ip_address`, `status`, `activated`, `permissions`, `last_login`, `created_at`, `updated_at`) VALUES
+(1, 'flavio', 'flaviozantut@gmail.com', 's8wX1nwxdqFUjl1ad529e5c42a25e13b217b7d75a2dfc2bf68609645ffe5309b25d00d3d516b1b32', '', '', 'MC4TGOBwXJwt07eY69d747dd322d22bfd236398de31f330b1bfe7f4ac16ecf2859603e26691d5de0', '', '127.0.0.1', '1', '1', '', '2012-09-09 21:23:31', '2012-09-07 20:49:28', '2012-09-09 21:23:31');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `user_id` int(10) unsigned NOT NULL,
+  `group_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users_metadata`
+--
+
+CREATE TABLE IF NOT EXISTS `users_metadata` (
+  `user_id` int(10) unsigned NOT NULL,
+  `first_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `users_metadata`
+--
+
+INSERT INTO `users_metadata` (`user_id`, `first_name`, `last_name`) VALUES
+(1, 'Flávio', 'Zantut');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users_suspended`
+--
+
+CREATE TABLE IF NOT EXISTS `users_suspended` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `login_id` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `attempts` int(11) NOT NULL,
+  `ip` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `last_attempt_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `suspended_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `unsuspend_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
